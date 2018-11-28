7d49ec36508ca7f62b11f514d57f1575436cf875
Cole Weston <colmwest@ut.utm.edu>
2018-11-27 18:21:48 -0600

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int sum(int num);
+
 int main(){
-  cout<< "hello world" << endl;
+  int number;
+  cout << "Enter a number: ";
+  cin >> number;
+  cout <<  sum(number) << endl;
   return 0;
 }
+
+int sum(int num){
+  int count = 0;
+  for(int i = 1; i < num+1; i++){
+    count += i;
+  }
+  return count;
+} 
 
 

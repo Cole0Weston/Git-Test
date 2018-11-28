b2300b7899db677227cc3438301d3d83fc76c88b
Cole Weston <colmwest@ut.utm.edu>
2018-11-27 18:36:22 -0600

    Feature Commit 1: main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int sum(int num);
+int prod(int num);
 
 int main(){
   int number;
   cout << "Enter a number: ";
   cin >> number;
-  cout <<  sum(number) << endl;
+  cout << "sum: " <<  sum(number) << endl
+       << "product: " << prod(number) << endl;
   return 0;
 }
 
@@ -15,6 +17,13 @@ int sum(int num){
   int count = 0;
   for(int i = 1; i < num+1; i++){
     count += i;
+  }
+  return count;
+}
+int prod(int num){
+  int count = 1;
+  for(int i = 1; i < num+1; i++){
+    count *= i;
   }
   return count;
 } 
